8328f753a66543add1983ef90aa189aafcc9ffff
Chance <tarver.chance@gmail.com>
2019-01-22 14:59:29 -0600

    Write basic unittest for PA class

--- tests/test_phypy.py
+++ tests/test_phypy.py
@@ -4,22 +4,14 @@
 """Tests for `phypy` package."""
 
 import pytest
+from phypy import analog
 
 
-from phypy import phypy
+def test_pa_error():
+    with pytest.raises(analog.PAError):
+        analog.PowerAmp(order=4)
 
 
-@pytest.fixture
-def response():
-    """Sample pytest fixture.
-
-    See more at: http://doc.pytest.org/en/latest/fixture.html
-    """
-    # import requests
-    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
-
-
-def test_content(response):
-    """Sample pytest test function with the pytest fixture as an argument."""
-    # from bs4 import BeautifulSoup
-    # assert 'GitHub' in BeautifulSoup(response.content).title.string
+def test_pa_setup():
+    pa = analog.PowerAmp(order=5)
+    assert pa.order == 5
