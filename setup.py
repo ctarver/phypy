aaa6885c48a41df23eec81f2ce2c1d86d474f073
root <root@chancelaptop.localdomain>
2019-09-26 09:50:36 -0500

    Bump version: 0.2.7 → 0.2.8

--- setup.py
+++ setup.py
@@ -46,6 +46,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/ctarver/phypy',
-    version='0.2.7',
+    version='0.2.8',
     zip_safe=False,
 )
